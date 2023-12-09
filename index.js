818081ccaa13a5afa7f971f22720492daf505119
Awiinn <awin@DESKTOP-V9FS76C>
2023-12-08 16:08:47 -0800

    working final workshop 15

--- index.js
+++ index.js
@@ -2,48 +2,64 @@ const userInputString = prompt(
   "What froyo would you like to order?",
   "vanilla, vanilla, vanilla, strawberry, coffee, coffee, coffee"
 );
-
-// Split the string of numbers into an array of strings.
+//splitting all user inputted values with a ,
 const stringArray = userInputString.split(",");
-// stringArray = ["1", "2", "3", "4", "5"]
-// Convert the array of strings into an array of numbers.
+//formed array of string values with a comma in between
 const flavors = [];
 for (let i = 0; i < stringArray.length; i++) {
   const flavor = stringArray[i];
-  flavors.push(flavor);
+  flavors.push(flavor.trim());
 }
 console.log(flavors);
 
-// const flavorVanilla = {
-//     name: "vanilla"
-// }
-
-// const flavorStrawberry = {
-//     name: "strawberry"
-// }
-const froyoFlavors = [
-  {
-    name: ["vanilla", "strawberry", "coffee"],
-  },
-];
-//if statement not working properly
-// function froyoAmount(flavors) {
-//   let amount = 0;
-//   for (let i = 0; i < flavors.length; i++) {
-//     if (flavors[i] === froyoFlavors[i]) amount++;
-//   }
-//   return amount;
-// }
-
-// console.log(`You have ordered ${froyoAmount(flavors)} froyos.`);
-
-function findCount(flavors, froyoFlavors) {
+function findCount(flavors) {
+  const froyoFlavors = {};
   let flavorCount = 0;
-  for (i = 0; i < flavors.length; i++)
-    if (flavors[i] === froyoFlavors.name) {
+  for (i = 0; i < flavors.length; i++) {
+    const flavor = flavors[i];
+    if (!(flavor in froyoFlavors)) {
+      froyoFlavors[flavor] = flavor;
       flavorCount++;
     }
-
+  }
   return flavorCount;
 }
-console.log("There are " + findCount(flavors) + "orders.");
+
+console.log("There are " + findCount(flavors) + " different flavors.");
+
+// finding how many vanilla are in the array
+function findVanilla(flavors) {
+  const flavorCount = {};
+  let count = 0;
+  flavors.forEach((val) => {
+    if (val === "vanilla") {
+      count++;
+    }
+  });
+  return count;
+}
+// finding how many strawberry are in the array
+function findStrawberry(flavors) {
+  const flavorCount = {};
+  let count = 0;
+  flavors.forEach((val) => {
+    if (val === "strawberry") {
+      count++;
+    }
+  });
+  return count;
+}
+// fiding how many coffee are in the array
+function findCoffee(flavors) {
+  const flavorCount = {};
+  let count = 0;
+  flavors.forEach((val) => {
+    if (val === "coffee") {
+      count++;
+    }
+  });
+  return count;
+}
+console.log("There are " + findVanilla(flavors) + " Vanilla froyos.");
+console.log("There are " + findStrawberry(flavors) + " Strawberry froyos.");
+console.log("There are " + findCoffee(flavors) + " Coffee froyos.");
